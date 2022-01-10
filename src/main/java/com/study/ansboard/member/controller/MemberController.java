b5abe114b002d88e4f22a895b3582507f82a2a5c
Kim Arum <83346234+kimrumm@users.noreply.github.com>
2022-01-10 18:30:49 +0900

    update : 회원가입 아이디, 비밀번호 유효성검사

--- src/main/java/com/study/ansboard/member/controller/MemberController.java
+++ src/main/java/com/study/ansboard/member/controller/MemberController.java
@@ -6,15 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/member")
@@ -28,7 +25,7 @@ public class MemberController {
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String getJoinForm() throws Exception {
 		logger.info("get LoginForm");
-		return "register/join";
+		return "/register/join";
 	}
 
 	// 회원가입 post
